1d3d2f904cedd34cceb93b3b64796f11ee22eb6f
nuiqueeljoe-afk <nuiqueeljoe@gmail.com>
2025-08-03 20:26:57 +0800

    Update 15_the_cyclone_1.py

--- 3-control-flow/15_the_cyclone_1.py
+++ 3-control-flow/15_the_cyclone_1.py
@@ -1,14 +1,15 @@
 # The Cyclone 🎢
 # Codédex
 
-height = int(input('What is your height (cm)? '))
-credits = int(input('How many credits do you have? '))
+height = int(input('What is your height (cm)?'))
+credits = int(input('How many credits do you have?'))
 
 if height >= 137 and credits >= 10:
-  print("Enjoy the ride!")
-elif height < 137 and credits >= 10:
-  print("You are not tall enough to ride.")
-elif credits < 10 and height >= 137:
-  print("You don't have enough credits to ride.")
+  print('Enjoy the ride')
+elif height < 137 and credits > 10:
+  print('You are not tall enought to ride')
+elif height > 137 and credits < 10:
+  print('You dont have enought credits')
 else:
-  print("You are not tall enough for this ride, nor do you have enough credits.")
+  print('they have not met either requirements')
+#so when you input both 137 and 10 it will execute "Enjoy the ride"
